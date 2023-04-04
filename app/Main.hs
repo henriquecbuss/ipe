ae60134d2bcaecc2f7d6848d2f1a202931bede7e
Henrique Buss <henriquebusss@gmail.com>
2023-04-03 22:49:22 -0300

    Add iris and fix Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,8 @@
 module Main (main) where
 
-import qualified Ipe
 import qualified System.Environment
 
 main :: IO ()
 main = do
   args <- System.Environment.getArgs
-  print $ Ipe.parseAssign $ head args
+  print args
